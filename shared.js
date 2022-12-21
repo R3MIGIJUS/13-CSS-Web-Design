21af69ba0b5a69105fb5216c55a90535aeb5d16d
R3MIGIJUS <remigijus.builys@gmail.com>
2022-12-21 23:42:34 +0200

    Create modal table and shared js

---
+++ shared.js
@@ -0,0 +1,21 @@
+let backdrop = document.querySelector(".backdrop");
+let modal = document.querySelector(".modal");
+let modalActionNo = document.querySelector(".modal__action--negative");
+let planButtons = document.querySelectorAll(".plan button");
+
+for (let i = 0; i < planButtons.length; i++) {
+  planButtons[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+});
+
+modalActionNo.addEventListener("click", function () {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+});
