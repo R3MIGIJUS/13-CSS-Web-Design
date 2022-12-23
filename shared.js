e9a6e2484553605e9a361b5d6de62bf215a12af1
R3MIGIJUS <remigijus.builys@gmail.com>
2022-12-23 22:43:14 +0200

    create mobile menu

--- shared.js
+++ shared.js
@@ -2,7 +2,8 @@ let backdrop = document.querySelector(".backdrop");
 let modal = document.querySelector(".modal");
 let modalActionNo = document.querySelector(".modal__action--negative");
 let planButtons = document.querySelectorAll(".plan button");
-let mobileVersion;
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
 
 for (let i = 0; i < planButtons.length; i++) {
   planButtons[i].addEventListener("click", function () {
@@ -14,9 +15,15 @@ for (let i = 0; i < planButtons.length; i++) {
 backdrop.addEventListener("click", function () {
   backdrop.style.display = "none";
   modal.style.display = "none";
+  mobileNav.style.display = "none";
 });
 
 modalActionNo.addEventListener("click", function () {
   backdrop.style.display = "none";
   modal.style.display = "none";
+});
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
 });
